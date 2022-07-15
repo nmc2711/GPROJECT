52c2f894291ea5c362ce928f1d24ba7b48e8cb2e
hwangsanghan <nmc2711@naver.com>
2022-07-15 15:08:43 +0900

    redux 추가

--- src/component/header/styles.ts
+++ src/component/header/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Header = styled.header`
   height: 42px;
+  background-color: #fff;
   border-bottom: 2px solid #00c01e;
   position: sticky;
   top: 0;
