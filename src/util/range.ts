dca9d29179629c41d9fde8c3e1f02ce86465cb46
hwang <nmc2711@naver.com>
2022-07-25 21:32:11 +0900

    all range

---
+++ src/util/range.ts
@@ -0,0 +1,17 @@
+function range(start: number, end?: number, step: number = 1) {
+    let output = [];
+  
+    if (typeof end === 'undefined') {
+      end = start;
+      start = 0;
+    }
+  
+    for (let i = start; i < end; i += step) {
+      output.push(i);
+    }
+  
+    return output;
+  }
+  
+  export default range;
+  
