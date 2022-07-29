ee84d5d2f1088215744b9035768429ce6fc27da6
hwang <nmc2711@naver.com>
2022-07-29 20:38:39 +0900

    sk fun

---
+++ src/util/sk.ts
@@ -0,0 +1,12 @@
+function makeObject<P>(obj: AnyObject): Partial<P> {
+    const keys = Object.keys(obj);
+  
+    let temp: Partial<P> = {};
+    keys.forEach(key => {
+      const value = obj[key];
+      splitChain<P>(key, value, temp);
+    });
+  
+    return temp;
+  }
+  
