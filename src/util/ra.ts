e5bc9fc617d19adda9859c71e2cced75af6afcb0
hwang <nmc2711@naver.com>
2022-08-14 18:35:24 +0900

    checkbox

--- src/util/ra.ts
+++
@@ -1,9 +0,0 @@
-import isEmpty from './valid/is-empty';
-
-function replaceAll(text: string, regStr: string, subStr: string) {
-  if (isEmpty(text)) return text;
-  const reg = new RegExp(`${regStr}`, 'gi');
-  return text.replace(reg, subStr);
-}
-
-export default replaceAll;
